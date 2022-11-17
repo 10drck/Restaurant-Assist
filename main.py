cc4c4de6ff2df31991033cd0c78727f9a5a62ee7
Kendrick M <93065586+10drck@users.noreply.github.com>
2022-11-17 16:09:49 -0500

    write file funct

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import csv
+
+def main():
+  #funct will call for functs and class
+  pass #<-temp
+
+
+def write_file(head_lst, data_lst):
+  """
+  using data found inside program writes to a csv file
+
+  args:
+    head_lst (lst): list containing the header names
+    data_lst (lst): list containing the data
+    can also be a dict
+  """
+  with open('.csv', 'w', encoding='UTF-8') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(head_lst) # write the header
+
+    writer.writerows(data_lst) # write mutiple rows.
+#can also use dicts 
+
+
+if __name__ == '__main__':
+  main()
+  #any other functs youd like to run during the call.
