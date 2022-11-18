1bdd13539c1eef1ee6e96f1993d6b5cc49bd2e8a
Sarah Peng <sarahpeng2003@gmail.com>
2022-11-17 23:27:52 -0500

    added customer class

--- main.py
+++ main.py
@@ -2,14 +2,40 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 
-#class Order():
+class Customers():
+  """Creates a customer object that can order food
   
-#class Receipt():
+  Attributes: 
+    Name(str): name of the customer
+    phone number (string): customers phone number
+    payment method (str): how they will pay
+  """
+  def __init__(self, name, phone, payment, order, order_num, time):
+    """Summary: takes in the name, phone number, payment method, order
+    
+    Args:
+        name (str): name of the customer
+        phone (str): _description_
+        payment (str): how they will pay
+        order (list): the customers order default to an empty list []
+        order_num (_type_): the order number 
+        time (_type_): the hour they ordered in 
+    
+    Side effects: 
+        assigns the name, phone number, payment method and order
+    """
+    pass
   
-#class Inventory():
+  def orders(order):
+    """Takes in the customers order and checks if it is available and
+        total cost of their bill
+        
+    Args:
+        order(lst): the order of the customer
+        
+    Returns: the cost of their bill and if there are any items not available
+    """
   
-#class Staff():
-
 def main():
   #funct will call for functs and class
   pass #<-temp
