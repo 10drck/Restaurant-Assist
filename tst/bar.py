0da1762bffa95d4e198c21dbc26c56581711ef8e
10drck <93065586+10drck@users.noreply.github.com>
2022-12-06 20:28:50 -0500

    bar.py update

--- tst/bar.py
+++ tst/bar.py
@@ -6,7 +6,7 @@ def main():
   df = pd.read_csv('restaurant-1-orders.csv')
   df = df.drop(['Order Number', 'Order Date', 'Product Price', 'Total products'], axis=1)
   df = df.groupby(['Item Name'], as_index=False, sort=False).sum().sort_values(by=['Quantity'], ascending=False)
-  print(df.head(3))
+  #print(df.head(3))
   
   plt.bar(x=df['Item Name'].head(7), height=df['Quantity'].head(7))
   plt.savefig('data.png')
