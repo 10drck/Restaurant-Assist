b329492b219e1559931fd8f514913167415c1afe
10drck <93065586+10drck@users.noreply.github.com>
2022-12-12 22:13:10 -0500

    del

--- main.py
+++ main.py
@@ -124,53 +124,6 @@ class RestaurantData():
           for order_id in bills:
               return f"{order_id}"
   
-  def check_availability(self, inventory):
-    """ uses list comprehension to add dictionary keys to a list in order to determine if an item is out of stock 
-       Args: 
-          inventory (dict):value: the remaining food in the restaurant keys: amount of the item (int)
-          order_list (lst): list of input orders 
-       Returns: 
-          updates the inventory dictionary 
-          prints out out of stock items 
-     """
-    
-    #create a list of the ordered items 
-    order_list = list(pd.read_csv('resturant-1-orders.csv', usecols = ["Item Name"]))
-    
-    #update the inventory for each order 
-    [inventory.values()-=1 for orders in order_list if inventory.values() >= 1] 
-    
-    #print out if an order is out of stock
-    for orders in order_list:
-      if inventory.values() == 0:
-        print(f"{inventory.keys()} is out of stock")
-
-  
-  def profit(self):
-    """Calculates the profit at the end of the month, creates a csv file with relevant information 
-    expenses - revenu (printed by the orders_total) 
-
-       Args: 
-            Orders_total (int): the combined total money from the orders 
-      Returns: 
-        profit as plain text 
-        monthly_profit(csv df): total profits for the day
-    """
-    #create the two dataframes out of the csv files
-    revenue_df = pd.read_csv('resturant-1-orders.csv', usecols = ["orders_total"])
-    cost_df = pd.read_csv("resturant_costs.csv")
-    
-    #calculate the profit 
-    profit = list(revenue_df.sum() - cost_df.sum())
-    
-    # return profit 
-    return profit 
-    #write to a new file (can't write to the main file, it isn't sorted by the order number)
-    write_file()
-    
-  
-  
-
 def write_file(head_lst, data_lst):
   """Using data from orders done in resturants, with the utilization of the pandas library
    be able to write to a csv to allow for spread sheet view.
